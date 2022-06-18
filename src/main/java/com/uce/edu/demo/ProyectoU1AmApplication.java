9714ebbd898c9b9d9bc5865aa0c827bd103dab08
Ariel Maldonado <arielsebas2000@gmail.com>
2022-06-17 20:10:53 -0500

    Subida del Taller 12 a GitHub.

--- src/main/java/com/uce/edu/demo/ProyectoU1AmApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU1AmApplication.java
@@ -1,37 +1,36 @@
 package com.uce.edu.demo;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.tarea10.modelo.TiendaVideojuegos;
-import com.uce.edu.demo.tarea10.modelo.Videojuego;
+import com.uce.edu.demo.modelo.Estudiante;
+import com.uce.edu.demo.modelo.Materia;
+import com.uce.edu.demo.modelo.Matricula;
+import com.uce.edu.demo.modelo.ProfesorGeneral;
+import com.uce.edu.demo.modelo.ProfesorMateria;
+import com.uce.edu.demo.service.IMatriculaService;
 
 @SpringBootApplication
 public class ProyectoU1AmApplication implements CommandLineRunner {
 
 	@Autowired
-	private TiendaVideojuegos store1;
+	private ProfesorGeneral profesor1;
 
 	@Autowired
-	private TiendaVideojuegos store2;
+	private ProfesorGeneral profesor2;
 
 	@Autowired
-	private Videojuego game1;
+	private ProfesorMateria mat1;
 
 	@Autowired
-	private Videojuego game2;
+	private ProfesorMateria mat2;
 
 	@Autowired
-	private Videojuego game3;
-
-	@Autowired
-	private Videojuego game4;
+	private IMatriculaService iMatriculaService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU1AmApplication.class, args);
@@ -43,58 +42,42 @@ public class ProyectoU1AmApplication implements CommandLineRunner {
 		// Singleton
 		System.out.println("\n  Singleton:");
 
-		this.store1.setRuc("121212");
-		this.store1.setNombre("Tierra del Gaming");
-		this.store1.setNombrePropietario("Ariel");
-		this.store1.setApellidoPropietario("Maldonado");
+		this.profesor1.setNombre("Ariel");
+		this.profesor1.setApellido("Maldonado");
 
-		System.out.println("Tienda 1:\n" + this.store1);
-		System.out.println("Tienda 2:\n" + this.store2);
+		System.out.println(this.profesor1);
 
-		this.store2.setNombre("Gaming Land");
+		System.out.println("  - - - - - - - ");
 
-		System.out.println("Tienda 1:\n" + this.store1);
-		System.out.println("Tienda 2:\n" + this.store2);
+		System.out.println(this.profesor2);
+		this.profesor2.setNombre("Raul");
+
+		System.out.println(" - - - - - - - ");
+
+		System.out.println(this.profesor1);
+
+		System.out.println("  - - - - - - - ");
+
+		System.out.println(this.profesor2);
 
 		// Prototype
 		System.out.println("\n  Prototype:");
 
-		this.game1.setNombre("God of War");
-		this.game1.setDesarrollador("Santa Monica Studio");
-		this.game1.setFechaLanzamiento(LocalDateTime.of(2018, 4, 20, 10, 0));
+		this.mat1.setNombre("Daniel");
+		this.mat1.setApellido("Terán");
 
-		System.out.println("Juego 1:\n" + this.game1);
-		System.out.println("Juego 2:\n" + this.game2);
+		System.out.println(this.mat1);
 
-		this.game2.setNombre("Days Gone");
-		this.game2.setDesarrollador("SIE Bend Studio");
-		this.game2.setFechaLanzamiento(LocalDateTime.of(2019, 4, 26, 16, 0));
+		System.out.println("  - - - - - - - ");
 
-		this.game3.setNombre("Spiderman PS4");
-		this.game3.setDesarrollador("Insomniac Games");
-		this.game3.setFechaLanzamiento(LocalDateTime.of(2018, 9, 7, 9, 0));
+		System.out.println(this.mat2);
 
-		this.game4.setNombre("Horizon Forbidden West");
-		this.game4.setDesarrollador("Guerrilla Games");
-		this.game4.setFechaLanzamiento(LocalDateTime.of(2022, 2, 18, 5, 0));
+		Matricula matricula1 = new Matricula();
+		matricula1.setEstudiante(new Estudiante());
+		matricula1.setMateria(new ArrayList<Materia>());
+		matricula1.setNumeroMatricula("13123");
 
-		System.out.println("\nDespues de hacer los set en todos los juegos cada bean tiene sus propios atributos:");
-		System.out.println("Juego 1:\n" + this.game1);
-		System.out.println("Juego 2:\n" + this.game2);
-		System.out.println("Juego 3:\n" + this.game3);
-		System.out.println("Juego 4:\n" + this.game4);
-
-		List<Videojuego> lista = new ArrayList<Videojuego>();
-		lista.add(game1);
-		lista.add(game2);
-		lista.add(game3);
-		lista.add(game4);
-
-		this.store2.setListaJuegos(lista);
-
-		System.out.println("\nEn ambas tiendas se añadira la lista de los 4 videojuegos: ");
-		System.out.println("Tienda 1:\n" + this.store1);
-		System.out.println("Tienda 2:\n" + this.store2);
+		this.iMatriculaService.ingresarMatricula(matricula1);
 
 	}
 
--- src/main/java/com/uce/edu/demo/service/MatriculaServiceImpl.java
+++ src/main/java/com/uce/edu/demo/service/MatriculaServiceImpl.java
@@ -1,6 +1,7 @@
 package com.uce.edu.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.stereotype.Service;
 
 import com.uce.edu.demo.modelo.Matricula;
@@ -16,15 +17,31 @@ public class MatriculaServiceImpl implements IMatriculaService {
 
 	@Autowired
 	private ProfesorGeneral profesorGeneral;
-	
+
 	@Autowired
 	private ProfesorMateria profesorMateria;
+
+//	@Lookup
+	public ProfesorGeneral obtenerProfesorG() {
+		return null;
+	}
+
+//	@Lookup
+	public ProfesorMateria obtenerProfesorM() {
+		ProfesorMateria profeM = new ProfesorMateria();
+		profeM.setNombre("Jose");
+		profeM.setApellido("Tapia");
+		return profeM;
+	}
 
 	@Override
 	public void ingresarMatricula(Matricula matricula) {
 		// TODO Auto-generated method stub
 		System.out.println("DI desde Service SINGLETON " + this.profesorGeneral);
 		System.out.println("DI desde Service PROTOTYPE " + this.profesorMateria);
+
+		System.out.println("DI un metodo GENERAL: " + this.obtenerProfesorG());
+		System.out.println("DI un metodo MATERIA: " + this.obtenerProfesorM());
 		this.matriculaRepository.insertar(matricula);
 	}
 
